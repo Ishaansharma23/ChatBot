52163fb60c74bd2df6d65082da56c7c4a1ad96ca
Ishaan Sharma <sharmaishaan0009@gmail.com>
2025-07-22 12:38:50 +0530

    removed base.html

--- app.py
+++ app.py
@@ -3,10 +3,6 @@ from chat import get_response
 
 app = Flask(__name__)
 
-@app.get("/")
-def index_get():
-    return render_template('base.html')
-
 @app.post("/predict")
 def predict():
     text = request.get_json().get("message")
